7f00588b6660a385f9efaec22604e284c0b0b212
JTHaugseth <97683128+JTHaugseth@users.noreply.github.com>
2023-03-02 17:00:38 +0100

    Enemy AI update

--- Assets/_Scripts/EnemyAI.cs
+++ Assets/_Scripts/EnemyAI.cs
@@ -6,96 +6,122 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     public NavMeshAgent agent;
-
     public Transform player;
+    public LayerMask obstacleMask;
 
-    public LayerMask whatIsGround, whatIsPlayer;
+    // Patrolling
+    private bool patrolling = true;
+    private Vector3 currentDestination;
+    public float patrolRange;
 
+    // Detection
+    public float detectionRange;
+    private bool playerDetected = false;
+    public LineRenderer lineRenderer;
 
-    //Patroling
-    public Vector3 walkPoint;
-    bool walkPointSet;
-    public float walkPointRange;
+    // Attack
+    public float attackRange;
+    public float attackDamage;
+    public float attackCooldown;
+    private bool canAttack = true;
 
-    //Attacking
-    public float timeBetweenAttacks;
-    bool alreadyAttacked;
-
-    //States
-    public float sightRange, attackRange;
-    public bool playerInSightRange, playerInAttackRange;
-
-    private void Awake()
+    private void Start()
     {
-        player = GameObject.Find("PlayerObj").transform;
-        agent = GetComponent<NavMeshAgent>();
+        currentDestination = GetRandomPositionInMaze();
+        agent.SetDestination(currentDestination);
+        lineRenderer.enabled = false;
     }
 
     private void Update()
     {
-        //Check for sight and attack range
-        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
-        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
-
-        if (!playerInSightRange && !playerInAttackRange) Patroling();
-        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
-        if (playerInAttackRange && playerInSightRange) AttackPlayer();
-    }
-
-    private void Patroling()
-    {
-        if (!walkPointSet) SearchWalkPoint();
-
-        if (walkPointSet)
-            agent.SetDestination(walkPoint);
-
-        Vector3 distanceToWalkPoint = transform.position - walkPoint;
-
-        //Walkpoint reached
-        if (distanceToWalkPoint.magnitude < 1f)
-            walkPointSet = false;
-    }
-    private void SearchWalkPoint()
-    {
-        //Calculate random point in range
-        float randomZ = Random.Range(-walkPointRange, walkPointRange);
-        float randomX = Random.Range(-walkPointRange, walkPointRange);
-
-        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
-
-        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
-            walkPointSet = true;
-    }
-
-    private void ChasePlayer()
-    {
-        agent.SetDestination(player.position);
-    }
-
-    private void AttackPlayer()
-    {
-        //Make sure enemy doesn't move
-        agent.SetDestination(transform.position);
-
-        transform.LookAt(player);
-
-        if (!alreadyAttacked)
+        if (patrolling)
         {
-            alreadyAttacked = true;
-            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+            // Check if we've reached the patrol destination
+            if (agent.remainingDistance < 0.5f)
+            {
+                // Set a new random destination
+                currentDestination = GetRandomPositionInMaze();
+                agent.SetDestination(currentDestination);
+            }
+
+            // Check if we can see the player
+            if (CanSeePlayer())
+            {
+                patrolling = false;
+                playerDetected = true;
+                lineRenderer.enabled = true;
+                Debug.Log("Player detected!");
+            }
+        }
+        else
+        {
+            // Move towards the player
+            agent.SetDestination(player.position);
+
+            // Check if we're close enough to attack
+            if (Vector3.Distance(transform.position, player.position) < attackRange)
+            {
+                if (canAttack)
+                {
+                    Attack();
+                    Debug.Log("Attack!");
+                }
+            }
+            else
+            {
+                // Check if we can still see the player
+                if (!CanSeePlayer()) {
+                    patrolling = true;
+                    playerDetected = false;
+                    lineRenderer.enabled = false;
+                    Debug.Log("Lost sight of player!");
+                }
+            }
+        }
+
+        // Update the line renderer if we can see the player
+        if (playerDetected) {
+            lineRenderer.SetPositions(new Vector3[] { transform.position, player.position });
         }
     }
-    private void ResetAttack()
-    {
-        alreadyAttacked = false;
+
+    private bool CanSeePlayer() {
+        // Check if the player is within detection range
+        if (Vector3.Distance(transform.position, player.position) > detectionRange) {
+            return false;
+        }
+
+        // Create a ray from the enemy to the player
+        Vector3 directionToPlayer = player.position - transform.position;
+        if (Physics.Raycast(transform.position, directionToPlayer, out RaycastHit hit, detectionRange)) {
+            // Check if the hit object is the player
+            if (hit.collider.gameObject.layer == LayerMask.NameToLayer("whatIsPlayer")) {
+                // Draw a line between the enemy and the player
+                Debug.DrawLine(transform.position, player.position, Color.green);
+                return true;
+            }
+        }
+        return false;
     }
 
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, attackRange);
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, sightRange);
+    private void Attack() {
+        canAttack = false;
+
+        // Set a cooldown before we can attack again
+        Invoke(nameof(ResetAttack), attackCooldown);
+    }
+
+    private void ResetAttack() {
+        canAttack = true;
+    }
+
+    private Vector3 GetRandomPositionInMaze() {
+        // Get a random position within the patrol range
+        Vector3 randomDirection = Random.insideUnitSphere * patrolRange;
+        randomDirection += transform.position;
+        NavMeshHit hit;
+        NavMesh.SamplePosition(randomDirection, out hit, patrolRange, 1);
+        return hit.position;
     }
 }
 
