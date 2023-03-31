033e380afe2ddd0b1db8673a81f7af9fdcb6320a
JTHaugseth <97683128+JTHaugseth@users.noreply.github.com>
2023-03-31 19:26:37 +0200

    Maze fixed update + camera

--- Assets/_Scripts/PlayerCamera.cs
+++ Assets/_Scripts/PlayerCamera.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
-
     // Mouse sensitivity
     public float sensX;
     public float sensY;
@@ -16,29 +15,49 @@ public class PlayerCamera : MonoBehaviour
 
     public PlayerHealth playerHealth;
 
+    private bool canProcessInput = false;
+
     // Start is called before the first frame update
     void Start() {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+
+        // Initialize rotation values
+        xRotation = WrapAngle(transform.eulerAngles.x);
+        yRotation = orientation.eulerAngles.y;
+
+        StartCoroutine(EnableInputProcessing());
+    }
+
+    IEnumerator EnableInputProcessing()
+    {
+        yield return new WaitForSeconds(0.5f);
+        canProcessInput = true;
     }
 
     // Update is called once per frame
     void Update() {
-        if (playerHealth.currentHealth > 0)
+        if (playerHealth.currentHealth > 0 && canProcessInput)
         {
-         // Get mouse input
-        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
-        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+            // Get mouse input
+            float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+            float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
 
-        yRotation += mouseX;
-        xRotation -= mouseY;
+            yRotation += mouseX;
+            xRotation -= mouseY;
 
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        // Rotate camera and orientation
-        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
-        orientation.rotation = Quaternion.Euler(0, yRotation, 0);   
+            // Rotate camera and orientation
+            transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+            orientation.rotation = Quaternion.Euler(0, yRotation, 0);
         }
-        
+    }
+
+    float WrapAngle(float angle) {
+        angle %= 360;
+        if (angle > 180)
+            return angle - 360;
+        return angle;
     }
 }
