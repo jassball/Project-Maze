3f8f1d1381402356d14739a3234939ea8a2a17d2
Vegardlarm <97906599+Vegardlarm@users.noreply.github.com>
2023-04-26 23:08:02 +0200

    update

---
+++ Assets/_Scripts/TorchSound.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchSound : MonoBehaviour
+{
+    public AudioClip soundClip1;
+    public AudioClip soundClip2;
+    public float volume; // Adjust this value in the Unity Editor to control volume
+
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = soundClip1;
+        audioSource.Play();
+    }
+
+    void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.clip = soundClip2;
+            audioSource.loop = true;
+            audioSource.volume = volume; // Set the volume of the second sound clip
+            audioSource.Play();
+        }
+    }
+}
